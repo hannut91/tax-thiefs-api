dc149901afa2ed8cff2ed693340f38de4d5afd9b
hannut91 <hannut91@gmail.com>
2019-12-07 12:50:49 +0900

    Add Dockerfile
    Add Dockerfile to dockerize the app.

---
+++ README.md
@@ -0,0 +1,5 @@
+# 내 주변의 세금도둑 API
+
+## Sources
+
+* https://www.nts.go.kr/openinfo/openinfo_03_01.asp
--- src/server.js
+++ src/server.js
@@ -2,6 +2,6 @@ import app from './app';
 
 const PORT = 80;
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Listening to requests on http://localhost:${PORT}`);
 });
